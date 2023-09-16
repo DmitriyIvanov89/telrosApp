de7870b03ab468d22d570b9ac5068c7939110656
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2023-09-16 13:13:47 +0300

    create new project

---
+++ src/main/java/com/divanov/telrosApp/TelrosAppApplication.java
@@ -0,0 +1,13 @@
+package com.divanov.telrosApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelrosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelrosAppApplication.class, args);
+	}
+
+}
