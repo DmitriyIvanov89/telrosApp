37519d9083fe697aa0de8314d4b166175ab807f7
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2023-11-23 22:51:48 +0300

    mod applayout

--- src/main/java/com/divanov/telrosApp/views/MainLayout.java
+++ src/main/java/com/divanov/telrosApp/views/MainLayout.java
@@ -4,6 +4,9 @@ import com.divanov.telrosApp.views.admin.AdminView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -34,12 +37,15 @@ public class MainLayout extends AppLayout {
     }
 
     private void createDrawer() {
-        RouterLink listLink = new RouterLink("List", AdminView.class);
+        VaadinIcon viewIcon = VaadinIcon.USER;
+        String viewName = "User's data";
+        Icon icon = viewIcon.create();
+        RouterLink listLink = new RouterLink(AdminView.class);
         listLink.setHighlightCondition(HighlightConditions.sameLocation());
+        listLink.add(icon, new Span(viewName));
 
         addToDrawer(new VerticalLayout(
                 listLink
         ));
     }
-
 }
